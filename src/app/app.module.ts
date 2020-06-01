58880069d390acc79fdee85604b7589fd00677d6
@Onyx777 <everth386@hotmail.com>
2020-05-31 19:39:37 -0500

    Componente de idiomas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { textosDin } from './perfil-usuario/perfilUsuario.component';
 import { MaterialModule } from './material/material.module';
 import { InformacionLaboralComponent } from './informacion-laboral/informacion-laboral.component';
+import { IdiomasComponent } from './idiomas/idiomas.component';
 
 
 
@@ -14,7 +15,8 @@ import { InformacionLaboralComponent } from './informacion-laboral/informacion-l
   declarations: [
     AppComponent,
     textosDin,
-    InformacionLaboralComponent
+    InformacionLaboralComponent,
+    IdiomasComponent
   ],
   imports: [
     BrowserModule,
